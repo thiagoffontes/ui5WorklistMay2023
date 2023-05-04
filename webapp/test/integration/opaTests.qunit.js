c9711fe5948b2dc62409d8481bca7a558d91dff2
thiago fontes <thiago.freire.fontes@accenture.com>
2023-05-04 12:43:18 +0000

    temaplate

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["treinamento/appcategories/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"treinamento/appcategories/test/unit/controller/List.controller"
+], function () {
+	"use strict";
+});
